aeaf2ef9531ae4b3cab57f135b01801fca425280
Esdayl <romain.chevallier@epita.fr>
2022-02-10 23:40:35 +0100

    Cleaner move
    Signed-off-by: Esdayl <romain.chevallier@epita.fr>

--- Project_SDL_Part1_base/derived/sheep.cpp
+++ Project_SDL_Part1_base/derived/sheep.cpp
@@ -43,23 +43,8 @@ void sheep::interact_with_object(std::shared_ptr<moving_object> obj)
         {
             set_dist(d);
             set_hunted(true);
-            int speed_x = get_x_speed();
-            int speed_y = get_y_speed();
-
-            if ((x < frame_boundary && speed_x < 0)
-                || (x > frame_width - frame_boundary && speed_x > 0))
-                boundary_x = true;
-            if ((y <= frame_boundary && speed_y < 0)
-                || (y >= frame_height - frame_boundary && speed_y > 0))
-                boundary_y = true;
-
-            if (!boundary_x)
-                speed_x = -((hunt_x - x) * speed) / d;
-            if (!boundary_y)
-                speed_y = -((hunt_y - y) * speed) / d;
-
-            set_x_speed(speed_x);
-            set_y_speed(speed_y);
+            set_x_speed(-((hunt_x - x) * speed) / d);
+            set_y_speed(-((hunt_y - y) * speed) / d);
         }
         return;
     }
@@ -76,33 +61,29 @@ void sheep::move()
     int x = get_x();
     int y = get_y();
     int speed = get_speed();
+    bool hunted = is_hunted();
     int speed_x = get_x_speed();
     int speed_y = get_y_speed();
-    bool hunted = is_hunted();
     bool boundary_y = false;
     bool boundary_x = false;
     float boost = 1.25;
 
-    if ((x < frame_boundary && speed_x < 0)
-        || (x > frame_width - frame_boundary && speed_x > 0))
+    boundary_x = ((x < frame_boundary && speed_x < 0)
+                  || (x > frame_width - frame_boundary && speed_x > 0));
+
+    if (!hunted && boundary_x)
     {
-        boundary_x = true;
-        if (!hunted)
-        {
-            set_x_speed(speed_x *= -1);
-            speed_x *= -1;
-        }
+        speed_x *= -1;
+        set_x_speed(speed_x);
     }
 
-    if ((y <= frame_boundary && speed_y < 0)
-        || (y >= frame_height - frame_boundary && speed_y > 0))
+    boundary_y = ((y <= frame_boundary && speed_y < 0)
+                  || (y >= frame_height - frame_boundary && speed_y > 0));
+
+    if (!hunted && boundary_y)
     {
-        boundary_y = true;
-        if (!hunted)
-        {
-            set_y_speed(speed_y *= -1);
-            speed_y *= -1;
-        }
+        speed_y *= -1;
+        set_y_speed(speed_y);
     }
 
     set_x(hunted ? (boundary_x ? x : x + speed_x * boost) : x + speed_x);
